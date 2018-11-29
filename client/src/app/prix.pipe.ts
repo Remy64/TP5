7c5ca6eb6a93dcd8c30b528bc99f5ff54bb3bf51
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-11-28 23:36:35 -0500

    pipe pour afficher les prix

---
+++ client/src/app/prix.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'prix'
+})
+export class PrixPipe implements PipeTransform {
+
+  transform(value: number): string {
+
+    return value.toFixed(2).replace('.', ',');
+  }
+
+}
