525310e8ebd9a5c22c171b2e95fe103262b2a3ee
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-12-04 18:49:03 -0500

    page de confirmation

--- client/src/app/confirmation/confirmation.component.ts
+++ client/src/app/confirmation/confirmation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService} from "../order.service";
 
 /**
 * Defines the component responsible to manage the confirmation page.
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
   selector: 'confirmation',
   templateUrl: './confirmation.component.html'
 })
-export class ConfirmationComponent {
+export class ConfirmationComponent implements  OnInit {
   // TODO: À compléter
+  numero: number;
+  nom: string;
+  prenom: string
+
+  constructor(private order: OrderService) { }
+  getOrder(): void {
+
+    this.order.getOrders().then(order => {this.numero = order.length;
+    this.nom = order[order.length - 1].lastname;
+    this.prenom = order[order.length - 1].firstname });
+
+  }
+
+ngOnInit() {
+
+  this.getOrder();
+}
+
+
 }
